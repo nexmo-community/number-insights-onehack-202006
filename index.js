2991752a83b5c269dc9757701eef938c78ae9b01
Chris Tankersley <chris@ctankersley.com>
2020-06-11 22:17:33 -0400

    Added nexmo code

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const Nexmo = require('nexmo');
 
 const app = express();
 
 const webRoute = async (request, response) => {
-    response.send('Hello World');
+    const nexmo = new Nexmo({
+        apiKey: process.env.NEXMO_API_KEY,
+        apiSecret: process.env.NEXMO_API_SECRET
+    });
+
+    const options = {
+        level: 'basic',
+        number: '14199564232'
+    };
+    
+    const basicHandler = (err, res) => {
+        response.send(res.country_name);
+    }
+    
+    nexmo.numberInsight.get(options, basicHandler);
 }
 
 app.get('/', webRoute);
