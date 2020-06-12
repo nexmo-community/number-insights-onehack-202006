d8ba8f8943e27fe4e3056a6a05a362ac1e8fdb27
Chris Tankersley <chris@ctankersley.com>
2020-06-12 11:11:45 -0400

    added lookup form

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const Nexmo = require('nexmo');
+const mustacheExpress = require('mustache-express');4
 
 const app = express();
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 const webRoute = async (request, response) => {
-    const nexmo = new Nexmo({
-        apiKey: process.env.NEXMO_API_KEY,
-        apiSecret: process.env.NEXMO_API_SECRET
-    });
-
-    const options = {
-        level: 'basic',
-        number: '14199564232'
-    };
+    if (request.query.number) {
+        const nexmo = new Nexmo({
+            apiKey: process.env.NEXMO_API_KEY,
+            apiSecret: process.env.NEXMO_API_SECRET
+        });
     
-    const basicHandler = (err, res) => {
-        response.send(res.country_name);
+        const options = {
+            level: request.query.level || 'basic',
+            number: request.query.number
+        };
+        
+        const basicHandler = (err, res) => {
+            if (res.status == 0) {
+                response.render('lookup', {insight: res});
+            } else {
+                response.render('lookup', {error: res});
+            }
+            
+        }
+        
+        nexmo.numberInsight.get(options, basicHandler);
+    } else {
+        response.render('lookup');
     }
     
-    nexmo.numberInsight.get(options, basicHandler);
 }
 
 app.get('/', webRoute);
